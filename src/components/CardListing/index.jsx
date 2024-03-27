08b15e5e4e9e1b02a861b54bc6897c74fbdfc929
Chris <63783051+CingSharped@users.noreply.github.com>
2024-03-27 08:52:15 +0000

    Small styling tweak

--- src/components/CardListing/index.jsx
+++ src/components/CardListing/index.jsx
@@ -3,6 +3,9 @@ import './style.css'
 function CardListing({ name, card_images, type, attribute, desc, atk, def, level , scale, linkval}) {
     const imageUrl = card_images[0].image_url_small;
 
+    // This will be used to display corresponding image to type of card, e.g. quickplay, counter, continuous, etc
+    // const spellAndTrapIdentifier = {"Quick-Play": "image-link"};
+
     return (
     <div className="card">
       <div className="card-image">
@@ -10,15 +13,15 @@ function CardListing({ name, card_images, type, attribute, desc, atk, def, level
       </div>
       <div className="card-info">
         <h2 className='card-name'>{name}</h2>
-        <p className='card-level'>{level}</p>
+        {level ? <p className='card-level'>{(type === "XYZ Monster" ? "Rank: " : "Level: ")}{level}</p> : <></>}
         {linkval ? <p className='card-linkval'>Link Val: {linkval}</p> : <></>}
         {scale ? <p className='card-scale'>Scale: {scale}</p> : <></>}
         <p className='card-type'>Type: {type}</p>
         <p className='card-desc'>{desc}</p>
         <p className='card-attribute'>Attribute: {attribute}</p>
         <div>
-            <p className='card-atk'>Atk{atk}</p>
-            <p className='card-def'>{def}</p>
+            <p className='card-atk'>Atk:{atk}</p>
+            {def ? <p className='card-def'>Def:{def}</p> : <></>}
         </div>
       </div>
     </div>

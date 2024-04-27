6a2e4f5c3bd7e7802f28977a6a7e68ede3f0bacc
christophersharpe <chrismsharpe123@gmail.com>
2024-04-27 10:38:54 +0100

    add prices to card controls element listing prices directly from card info.

--- src/components/CardListing/index.jsx
+++ src/components/CardListing/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './style.css'
 
-function CardListing({ name, card_images, type, attribute, desc, atk, def, level , scale, linkval}) {
+function CardListing({ name, card_images, type, attribute, desc, atk, def, level , scale, linkval, card_prices}) {
     const imageUrl = card_images[0].image_url_small;
     const [clicked,setClicked] = useState(false);
 
@@ -13,41 +13,47 @@ function CardListing({ name, card_images, type, attribute, desc, atk, def, level
 
     return (
       <div>
-      <div className="card" onClick={() => userClicked()}>
-        <div className="card-image">
-          <img src={imageUrl} alt="" />
-        </div>
-        <div className="card-info">
-          <h2 className="card-name">{name}</h2>
-          <div className="card-stats">
-            {level ? (
-              <p className="card-level">
-                {type === "XYZ Monster" ? "Rank: " : "Level: "}
-                {level}
-              </p>
-            ) : (
-              <></>
-            )}
-            {linkval ? (
-              <p className="card-linkval">Link Val: {linkval}</p>
-            ) : (
-              <></>
-            )}
-            {scale ? <p className="card-scale">Scale: {scale}</p> : <></>}
-            <p className="card-type">Type: {type}</p>
-            <p className="card-attribute">Attribute: {attribute}</p>
-            <p className="card-atk">Atk:{atk}</p>
-            {def ? <p className="card-def">Def:{def}</p> : <></>}
+        <div className="card" onClick={() => userClicked()}>
+          <div className="card-image">
+            <img src={imageUrl} alt="" />
           </div>
-          <p className="card-desc">{desc}</p>
+          <div className="card-info">
+            <h2 className="card-name">{name}</h2>
+            <div className="card-stats">
+              {level ? (
+                <p className="card-level">
+                  {type === "XYZ Monster" ? "Rank: " : "Level: "}
+                  {level}
+                </p>
+              ) : (
+                <></>
+              )}
+              {linkval ? (
+                <p className="card-linkval">Link Val: {linkval}</p>
+              ) : (
+                <></>
+              )}
+              {scale ? <p className="card-scale">Scale: {scale}</p> : <></>}
+              <p className="card-type">Type: {type}</p>
+              <p className="card-attribute">Attribute: {attribute}</p>
+              <p className="card-atk">Atk:{atk}</p>
+              {def ? <p className="card-def">Def:{def}</p> : <></>}
+            </div>
+            <p className="card-desc">{desc}</p>
+          </div>
         </div>
-      </div>
         {clicked ? (
-          <div className='card-controls'>Hellooooooooooooooooooooooooooooooooooooooooooooooooooo</div>
+          <div className="card-controls">
+            <p>Cardmarket: £{card_prices[0].cardmarket_price}</p>
+            <p>TCG Player: £{card_prices[0].tcgplayer_price}</p>
+            <p>Ebay: £{card_prices[0].ebay_price}</p>
+            <p>Amazon: £{card_prices[0].amazon_price}</p>
+            <p>Cool Stuff Inc: £{card_prices[0].coolstuffinc_price}</p>
+          </div>
         ) : (
           <></>
         )}
-        </div>
+      </div>
     );
 }
 

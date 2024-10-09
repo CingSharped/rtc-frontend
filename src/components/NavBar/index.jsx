e569a20ba9a95a7c0104dcc1c3c031d3628859e5
Chris <63783051+CingSharped@users.noreply.github.com>
2024-10-09 19:14:39 +0100

    add app wide navigation button styling

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,16 +6,31 @@ const NavBar = () => {
   return (
     <header>
       <nav>
-        <div className='nav-links'>
-
-          <NavLink to="/">Home</NavLink>
-          <NavLink to='/dashboard'>Dashboard</NavLink>
-          <NavLink to='/login'>Login</NavLink>
-          <NavLink to='/signup'>Signup</NavLink>
-          <NavLink to='/profile'>Profile</NavLink>
-          <NavLink to='/cardsearch'>Card Search</NavLink>
-          <NavLink to='/support'>Support</NavLink>
-          <NavLink to='/about'>About Us</NavLink>
+        <div className="nav-links">
+          <NavLink className="custom-btn nav-button" to="/">
+            Home
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/dashboard">
+            Dashboard
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/signup">
+            Signup
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/profile">
+            Profile
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/cardsearch">
+            Card Search
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/support">
+            Support
+          </NavLink>
+          <NavLink className="custom-btn nav-button" to="/about">
+            About Us
+          </NavLink>
         </div>
       </nav>
       <Outlet />
